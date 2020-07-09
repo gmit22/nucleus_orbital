2662a294447897deebb7d5f3d9fa6e8f46c2d9cf
gmit22 <59180102+gmit22@users.noreply.github.com>
2020-07-09 18:22:56 +0530

    upcoming bookings added to db

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,9 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
+from accounts.models import BookingManager
+
+
 def index(request):
 	return render(request, 'accounts/login.html')
 
@@ -10,9 +13,12 @@ def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
+
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
+            bm = BookingManager(userid=username, upcoming_bookings="", past_bookings="")
+            bm.save()
             user = authenticate(username=username, password=raw_password)
             login(request, user)
             return redirect(index)
