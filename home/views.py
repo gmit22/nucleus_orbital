5b6c8491ef98516a62d1faf81aff1a00198bd3e1
gmit22 <59180102+gmit22@users.noreply.github.com>
2020-07-14 03:51:49 +0530

    views.py updated

--- home/views.py
+++ home/views.py
@@ -6,31 +6,41 @@ from .models import Sport, Booking
 import datetime
 from .forms import tabletennis, basketball, squash
 
-
-def index(request):
-
+def past_bookings(request):
     username = None
     if request.user.is_authenticated:
         username = request.user.username
 
-    # admin = BookingManager(userid='admin', past_bookings="", upcoming_bookings="")
-    # admin.save()
-    #
-    # output = "University Sports Center, Court 4 0700 : 0800|University Sports Center, Court 4 0800 : 0900|University Sports Center, Court 4 0900 : 1000|University Sports Center, Court 4 1000 : 1100|University Sports Center, Court 4 1100 : 1200|University Sports Center, Court 4 1200 : 1300|University Sports Center, Court 4 1300 : 1400|University Sports Center, Court 4 1400 : 1500|University Sports Center, Court 4 1500 : 1600|University Sports Center, Court 4 1600 : 1700|University Sports Center, Court 4 1700 : 1800|University Sports Center, Court 3 0800 : 0900|University Sports Center, Court 3 0900 : 1000|University Sports Center, Court 3 1000 : 1100|University Sports Center, Court 3 1100 : 1200|University Sports Center, Court 3 1200 : 1300|University Sports Center, Court 3 1300 : 1400|University Sports Center, Court 3 1400 : 1500|University Sports Center, Court 3 1500 : 1600|University Sports Center, Court 3 1600 : 1700|University Sports Center, Court 3 1700 : 1800|University Sports Center, Court 3 1800 : 1900|University Sports Center, Court 2 0900 : 1000|University Sports Center, Court 2 1000 : 1100|University Sports Center, Court 2 1100 : 1200|University Sports Center, Court 2 1200 : 1300|University Sports Center, Court 2 1300 : 1400|University Sports Center, Court 2 1400 : 1500|University Sports Center, Court 2 1500 : 1600|University Sports Center, Court 2 1600 : 1700|University Sports Center, Court 2 1700 : 1800|University Sports Center, Court 2 1800 : 1900|University Sports Center, Court 2 1900 : 2000|University Sports Center, Court 1 1000 : 1100|University Sports Center, Court 1 1100 : 1200|University Sports Center, Court 1 1200 : 1300|University Sports Center, Court 1 1300 : 1400|University Sports Center, Court 1 1400 : 1500|University Sports Center, Court 1 1500 : 1600|University Sports Center, Court 1 1600 : 1700|University Sports Center, Court 1 1700 : 1800|University Sports Center, Court 1 1800 : 1900|University Sports Center, Court 1 1900 : 2000|University Sports Center, Court 1 2000 : 2100|Caged Court, Prince George's Park Residences 0700 : 0800|Caged Court, Prince George's Park Residences 0800 : 0900|Caged Court, Prince George's Park Residences 0900 : 1000|Caged Court, Prince George's Park Residences 1000 : 1100|Caged Court, Prince George's Park Residences 1100 : 1200|Caged Court, Prince George's Park Residences 1200 : 1300|Caged Court, Prince George's Park Residences 1300 : 1400|Caged Court, Prince George's Park Residences 1400 : 1500|Caged Court, Prince George's Park Residences 1500 : 1600|Caged Court, Prince George's Park Residences 1600 : 1700|Caged Court, Prince George's Park Residences 1700 : 1800|Court 1, Prince George's Park Residences 0800 : 0900|Court 1, Prince George's Park Residences 0900 : 1000|Court 1, Prince George's Park Residences 1000 : 1100|Court 1, Prince George's Park Residences 1100 : 1200|Court 1, Prince George's Park Residences 1200 : 1300|Court 1, Prince George's Park Residences 1300 : 1400|Court 1, Prince George's Park Residences 1400 : 1500|Court 1, Prince George's Park Residences 1500 : 1600|Court 1, Prince George's Park Residences 1600 : 1700|Court 1, Prince George's Park Residences 1700 : 1800|Court 1, Prince George's Park Residences 1800 : 1900|Court 2, Prince George's Park Residences 0900 : 1000|Court 2, Prince George's Park Residences 1000 : 1100|Court 2, Prince George's Park Residences 1100 : 1200|Court 2, Prince George's Park Residences 1200 : 1300|Court 2, Prince George's Park Residences 1300 : 1400|Court 2, Prince George's Park Residences 1400 : 1500|Court 2, Prince George's Park Residences 1500 : 1600|Court 2, Prince George's Park Residences 1600 : 1700|Court 2, Prince George's Park Residences 1700 : 1800|Court 2, Prince George's Park Residences 1800 : 1900|Court 2, Prince George's Park Residences 1900 : 2000|Main Court, University Sports Centre 1000 : 1100|Main Court, University Sports Centre 1100 : 1200|Main Court, University Sports Centre 1200 : 1300|Main Court, University Sports Centre 1300 : 1400|Main Court, University Sports Centre 1400 : 1500|Main Court, University Sports Centre 1500 : 1600|Main Court, University Sports Centre 1600 : 1700|Main Court, University Sports Centre 1700 : 1800|Main Court, University Sports Centre 1800 : 1900|Main Court, University Sports Centre 1900 : 2000|Main Court, University Sports Centre 2000 : 2100|Table Tennis : Prince George's Park Residences, PGPH 1200 : 1300|Table Tennis : Prince George's Park Residences, PGPH 1300 : 1400|Table Tennis : Prince George's Park Residences, PGPH 1400 : 1500|Table Tennis : Prince George's Park Residences, PGPH 1500 : 1600"
-    # slots = ''
-    # peer_reqd = ''
-    #
-    # for i in range(0, 93):
-    #     slots += 'free|'
-    #     peer_reqd += 'yes|'
-    #
-    # for i in range(0, 365):
-    #     print("today's date", datetime.date.today)
-    #     trial = Booking(dt=datetime.date.today() + datetime.timedelta(days=i), lt=output, st=slots[:-1],
-    #                     peer=peer_reqd[:-1])
-    #     trial.save()
+    past_booking_output = []
+    temp = BookingManager.objects.get(userid=username)
+    bookings = temp.upcoming_bookings.split('|')[:-1]
 
-    booking_output = []
+    for booking in bookings:
+
+        booking_dict = {}
+        date = datetime.datetime.strptime(booking[0:10], "%Y-%m-%d")
+        now = datetime.datetime.now()
+
+        # create a dictionary to be used in HTML file
+        booking_dict['name'] = booking
+
+        booking_objects = Booking.objects.get(dt=date.date())
+        bi = booking_objects.lt.split('|')
+        booking_index = bi.index(booking[11:])
+        sts = booking_objects.st.split('|')[:-1][booking_index].split(',')[:-1]
+        booking_dict['users'] = ["users booked:"] + sts
+
+        if date < now:
+            past_booking_output.append(booking_dict)
+
+    return past_booking_output
+
+def upcoming_booking(request):
+    username = None
+    if request.user.is_authenticated:
+        username = request.user.username
+
+    upcoming_booking_output = []
     temp = BookingManager.objects.get(userid=username)
     bookings = temp.upcoming_bookings.split('|')[:-1]
 
@@ -53,8 +63,32 @@ def index(request):
             sts = booking_objects.st.split('|')[:-1][booking_index].split(',')[:-1]
             booking_dict['users'] = ["users booked:"] + sts
 
-            booking_output.append(booking_dict)
+            upcoming_booking_output.append(booking_dict)
+    return upcoming_booking_output
 
+def index(request):
+    print(10)
+    username = None
+    if request.user.is_authenticated:
+        username = request.user.username
+
+    # admin = BookingManager(userid='admin', past_bookings="", upcoming_bookings="")
+    # admin.save()
+    #
+    # output = "University Sports Center, Court 4 0700 : 0800|University Sports Center, Court 4 0800 : 0900|University Sports Center, Court 4 0900 : 1000|University Sports Center, Court 4 1000 : 1100|University Sports Center, Court 4 1100 : 1200|University Sports Center, Court 4 1200 : 1300|University Sports Center, Court 4 1300 : 1400|University Sports Center, Court 4 1400 : 1500|University Sports Center, Court 4 1500 : 1600|University Sports Center, Court 4 1600 : 1700|University Sports Center, Court 4 1700 : 1800|University Sports Center, Court 3 0800 : 0900|University Sports Center, Court 3 0900 : 1000|University Sports Center, Court 3 1000 : 1100|University Sports Center, Court 3 1100 : 1200|University Sports Center, Court 3 1200 : 1300|University Sports Center, Court 3 1300 : 1400|University Sports Center, Court 3 1400 : 1500|University Sports Center, Court 3 1500 : 1600|University Sports Center, Court 3 1600 : 1700|University Sports Center, Court 3 1700 : 1800|University Sports Center, Court 3 1800 : 1900|University Sports Center, Court 2 0900 : 1000|University Sports Center, Court 2 1000 : 1100|University Sports Center, Court 2 1100 : 1200|University Sports Center, Court 2 1200 : 1300|University Sports Center, Court 2 1300 : 1400|University Sports Center, Court 2 1400 : 1500|University Sports Center, Court 2 1500 : 1600|University Sports Center, Court 2 1600 : 1700|University Sports Center, Court 2 1700 : 1800|University Sports Center, Court 2 1800 : 1900|University Sports Center, Court 2 1900 : 2000|University Sports Center, Court 1 1000 : 1100|University Sports Center, Court 1 1100 : 1200|University Sports Center, Court 1 1200 : 1300|University Sports Center, Court 1 1300 : 1400|University Sports Center, Court 1 1400 : 1500|University Sports Center, Court 1 1500 : 1600|University Sports Center, Court 1 1600 : 1700|University Sports Center, Court 1 1700 : 1800|University Sports Center, Court 1 1800 : 1900|University Sports Center, Court 1 1900 : 2000|University Sports Center, Court 1 2000 : 2100|Caged Court, Prince George's Park Residences 0700 : 0800|Caged Court, Prince George's Park Residences 0800 : 0900|Caged Court, Prince George's Park Residences 0900 : 1000|Caged Court, Prince George's Park Residences 1000 : 1100|Caged Court, Prince George's Park Residences 1100 : 1200|Caged Court, Prince George's Park Residences 1200 : 1300|Caged Court, Prince George's Park Residences 1300 : 1400|Caged Court, Prince George's Park Residences 1400 : 1500|Caged Court, Prince George's Park Residences 1500 : 1600|Caged Court, Prince George's Park Residences 1600 : 1700|Caged Court, Prince George's Park Residences 1700 : 1800|Court 1, Prince George's Park Residences 0800 : 0900|Court 1, Prince George's Park Residences 0900 : 1000|Court 1, Prince George's Park Residences 1000 : 1100|Court 1, Prince George's Park Residences 1100 : 1200|Court 1, Prince George's Park Residences 1200 : 1300|Court 1, Prince George's Park Residences 1300 : 1400|Court 1, Prince George's Park Residences 1400 : 1500|Court 1, Prince George's Park Residences 1500 : 1600|Court 1, Prince George's Park Residences 1600 : 1700|Court 1, Prince George's Park Residences 1700 : 1800|Court 1, Prince George's Park Residences 1800 : 1900|Court 2, Prince George's Park Residences 0900 : 1000|Court 2, Prince George's Park Residences 1000 : 1100|Court 2, Prince George's Park Residences 1100 : 1200|Court 2, Prince George's Park Residences 1200 : 1300|Court 2, Prince George's Park Residences 1300 : 1400|Court 2, Prince George's Park Residences 1400 : 1500|Court 2, Prince George's Park Residences 1500 : 1600|Court 2, Prince George's Park Residences 1600 : 1700|Court 2, Prince George's Park Residences 1700 : 1800|Court 2, Prince George's Park Residences 1800 : 1900|Court 2, Prince George's Park Residences 1900 : 2000|Main Court, University Sports Centre 1000 : 1100|Main Court, University Sports Centre 1100 : 1200|Main Court, University Sports Centre 1200 : 1300|Main Court, University Sports Centre 1300 : 1400|Main Court, University Sports Centre 1400 : 1500|Main Court, University Sports Centre 1500 : 1600|Main Court, University Sports Centre 1600 : 1700|Main Court, University Sports Centre 1700 : 1800|Main Court, University Sports Centre 1800 : 1900|Main Court, University Sports Centre 1900 : 2000|Main Court, University Sports Centre 2000 : 2100|Table Tennis : Prince George's Park Residences, PGPH 1200 : 1300|Table Tennis : Prince George's Park Residences, PGPH 1300 : 1400|Table Tennis : Prince George's Park Residences, PGPH 1400 : 1500|Table Tennis : Prince George's Park Residences, PGPH 1500 : 1600"
+    # slots = ''
+    # peer_reqd = ''
+    #
+    # for i in range(0, 93):
+    #     slots += 'free|'
+    #     peer_reqd += 'yes|'
+    #
+    # for i in range(0, 365):
+    #     print("today's date", datetime.date.today)
+    #     trial = Booking(dt=datetime.date.today() + datetime.timedelta(days=i), lt=output, st=slots[:-1],
+    #                     peer=peer_reqd[:-1])
+    #     trial.save()
+    booking_output = upcoming_booking(request)
     return render(request, 'home/Navbar.html', {"username": username, "bookings": booking_output})
 
     # temp = Sport.objects.all()
@@ -90,7 +124,6 @@ def index(request):
     #         file.write(array)
     #         output += array
 
-
 def sports(request):
     temp = Sport.objects.all()
     sports = []
@@ -108,39 +141,13 @@ def sports(request):
 
     return render(request, 'home/facilities.html', {'sports': sports})
 
-
 def bookSlot(request, sport):
     username = None
     if request.user.is_authenticated:
         username = request.user.username
 
-
-    past_booking_output = []
-    booking_output = []
     temp = BookingManager.objects.get(userid=username)
-    bookings = temp.upcoming_bookings.split('|')[:-1]
-
-    for booking in bookings:
-
-        booking_dict = {}
-
-        # print(booking_date)
-        date = datetime.datetime.strptime(booking[0:10], "%Y-%m-%d")
-        now = datetime.datetime.now()
-
-        # create a dictionary to be used in HTML file
-        booking_dict['name'] = booking
-
-        booking_objects = Booking.objects.get(dt=date.date())
-        bi = booking_objects.lt.split('|')
-        booking_index = bi.index(booking[11:])
-        sts = booking_objects.st.split('|')[:-1][booking_index].split(',')[:-1]
-        booking_dict['users'] = ["users booked:"] + sts
-
-        if date > now:
-            booking_output.append(booking_dict)
-        else:
-            past_booking_output.append(booking_dict)
+    past_booking = past_bookings(request)
 
     if request.method == 'POST':
 
@@ -160,7 +167,7 @@ def bookSlot(request, sport):
                     slots[i] = ''
 
                 if peers[i] == 'no':
-                    return render(request, 'home/nobooking.html', {"past_bookings": past_booking_output})
+                    return render(request, 'home/nobooking.html', {"past_bookings": past_booking})
 
                 if username not in slots[i]:
                     slots[i] += username + ','
@@ -189,7 +196,7 @@ def bookSlot(request, sport):
                     slots[i] = ''
 
                 if peers[i] == 'no':
-                    return render(request, 'home/nobooking.html')
+                    return render(request, 'home/nobooking.html', {"past_bookings": past_booking})
 
                 if username not in slots[i]:
                     slots[i] += username + ','
@@ -208,7 +215,6 @@ def bookSlot(request, sport):
                 current = Booking.objects.get(dt=form['date'])
                 pr = form['peer_reqd']
                 print(pr)
-                print('squash is working')
                 peers = current.peer.split('|')
                 lts = current.lt.split('|')
                 slots = current.st.split('|')
@@ -218,7 +224,7 @@ def bookSlot(request, sport):
                     slots[i] = ''
 
                 if peers[i] == 'no':
-                    return render(request, 'home/nobooking.html')
+                    return render(request, 'home/nobooking.html', {"past_bookings": past_booking})
 
                 if username not in slots[i]:
                     slots[i] += username + ','
@@ -230,9 +236,7 @@ def bookSlot(request, sport):
                 current.peer = '|'.join(peers)
                 current.save()
 
-        # to get all the valid upcoming bookings
-
-
+        booking_output = upcoming_booking(request)
 
         return render(request, 'home/bookingsuccessful.html', {"username": username, "bookings": booking_output})
 
